88a4a51a8fa20a5df4907ccdb00a11eff70e54b5
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 17:10:44 +0530

    Update main.py

--- 15_Deque/15.3_Sliding_Window_Maximum/main.py
+++ 15_Deque/15.3_Sliding_Window_Maximum/main.py
@@ -3,15 +3,31 @@
 # Type: In-Session
 
 from collections import deque
-from typing import List
 
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        pass
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        dq = deque()  # will store indices
+        res = []
+
+        for i in range(len(nums)):
+            # Remove elements out of current window
+            if dq and dq[0] < i - k + 1:
+                dq.popleft()
+
+            # Maintain decreasing order in deque
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+
+            dq.append(i)
+
+            # Once we reach window size k, record max
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+
+        return res
 
 # Demo
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))  
-    print(sol.maxSlidingWindow([9,5,3,1,2,8], 2))      
-    print(sol.maxSlidingWindow([2,1], 2))               
+    print(sol.maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))  # [3, 3, 5, 5, 6, 7]
+    print(sol.maxSlidingWindow([1], 1))                        # [1]
