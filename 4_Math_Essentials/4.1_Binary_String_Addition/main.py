00d1c9f3d1effc7baffcc3e659d93fbee9084494
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 15:12:57 +0530

    Add binary string addition implementation
    Implemented binary string addition algorithm in addBinary method.

--- 4_Math_Essentials/4.1_Binary_String_Addition/main.py
+++ 4_Math_Essentials/4.1_Binary_String_Addition/main.py
@@ -1,6 +1,19 @@
-# Binary String Addition
-# Topic: Math Essentials
-# Type: In-Session
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        while i >= 0 or j >= 0 or carry:
+            sum_val = carry
+            if i >= 0:
+                sum_val += int(a[i])
+                i -= 1
+            if j >= 0:
+                sum_val += int(b[j])
+                j -= 1
+            res = str(sum_val % 2) + res
+            carry = sum_val // 2
+        return res
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
