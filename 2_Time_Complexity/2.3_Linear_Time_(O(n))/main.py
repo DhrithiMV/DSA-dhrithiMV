f037d3f60de362c360ba4d0dcbcf1e2b9495fffa
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:22:39 +0530

    Update main.py

--- 2_Time_Complexity/2.3_Linear_Time_(O(n))/main.py
+++ 2_Time_Complexity/2.3_Linear_Time_(O(n))/main.py
@@ -1,16 +1,5 @@
-# Linear Time (O(n))
-# Topic: Time & Space Complexity
-# Type: In-Session
-
-class Solution:
-    def sumArray(self, arr: list[int]) -> int:
-        pass
-
-# Demo
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.sumArray([2,4,6,8]))      # Output: 20
-    print(sol.sumArray([1]))            # Output: 1
-    print(sol.sumArray([-5,10,-3]))     # Output: 2
-    print(sol.sumArray([0,0,0,0]))      # Output: 0
-
+def array_sum(arr):
+    total = 0
+    for num in arr:
+        total += num
+    return total
