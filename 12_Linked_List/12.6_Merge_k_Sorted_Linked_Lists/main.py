026ac684c1d858d64a48032224f881900219eae7
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:54:42 +0530

    Update main.py

--- 12_Linked_List/12.6_Merge_k_Sorted_Linked_Lists/main.py
+++ 12_Linked_List/12.6_Merge_k_Sorted_Linked_Lists/main.py
@@ -1,30 +1,57 @@
 import heapq
 
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, key=0, val=0):
+        self.key = key
         self.val = val
-        self.next = next
-    
-    def __lt__(self, other):
-        # Define less than for ListNode to be used in heapq
-        return self.val < other.val
+        self.prev = None
+        self.next = None
 
-class Solution:
-    def mergeKLists(self, lists: list) -> ListNode:
-        pass
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.left, self.right = ListNode(), ListNode()
+        self.left.next = self.right
+        self.right.prev = self.left
 
-# Demo (simple example)
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node):
+        prev = self.right.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.right
+        self.right.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = ListNode(key, value)
+        self.insert(self.cache[key])
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
 if __name__ == "__main__":
-    # Construct linked lists: [1->4->5], [1->3->4], [2->6]
-    l1 = ListNode(1, ListNode(4, ListNode(5)))
-    l2 = ListNode(1, ListNode(3, ListNode(4)))
-    l3 = ListNode(2, ListNode(6))
-    
-    sol = Solution()
-    merged = sol.mergeKLists([l1, l2, l3])
-    
-    # Print merged list
-    curr = merged
-    while curr:
-        print(curr.val, end=" -> " if curr.next else "")
-        curr = curr.next
+    lru = LRUCache(2)
+    lru.put(1, 1)
+    lru.put(2, 2)
+    print(lru.get(1))
+    lru.put(3, 3)
+    print(lru.get(2))
+    lru.put(4, 4)
+    print(lru.get(1))
+    print(lru.get(3))
+    print(lru.get(4))
