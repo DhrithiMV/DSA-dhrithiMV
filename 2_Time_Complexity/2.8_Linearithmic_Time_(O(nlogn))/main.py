6f03d8102ae765cb985591023a609012022205c0
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:33:41 +0530

    Update main.py

--- 2_Time_Complexity/2.8_Linearithmic_Time_(O(nlogn))/main.py
+++ 2_Time_Complexity/2.8_Linearithmic_Time_(O(nlogn))/main.py
@@ -1,17 +1,24 @@
-# Linearithmic Time (O(nlogn))
-# Topic: Time & Space Complexity
-# Type: Home Challenge
-
-class Solution:
-    def halveAndPrint(self, n: int) -> None:
-        pass
-# Demo
-if __name__ == '__main__':
-    sol = Solution()
-    sol.halveAndPrint(8)
-    # Output:
-    # 1 2 3 4 5 6 7 8
-    # 1 2 3 4
-    # 1 2
-    # 1
-
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
