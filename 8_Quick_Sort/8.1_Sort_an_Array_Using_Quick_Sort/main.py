fe5f053003843406ae7f106ac879af2c6b90abdc
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:03:38 +0530

    Update main.py

--- 8_Quick_Sort/8.1_Sort_an_Array_Using_Quick_Sort/main.py
+++ 8_Quick_Sort/8.1_Sort_an_Array_Using_Quick_Sort/main.py
@@ -1,15 +1,15 @@
-# Sort an Array Using Quick Sort
-# Topic: Quick Sort
-# Type: In-Session
-
-from typing import List
-
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        pass
-
-# Demo
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.sortArray([5,2,3,1]))  
-    print(sol.sortArray([10,7,8,9,1,5])) 
+    def quickSort(self, arr, low, high):
+        if low < high:
+            pi = self.partition(arr, low, high)
+            self.quickSort(arr, low, pi - 1)
+            self.quickSort(arr, pi + 1, high)
+    def partition(self, arr, low, high):
+        pivot = arr[high]
+        i = low - 1
+        for j in range(low, high):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i + 1], arr[high] = arr[high], arr[i + 1]
+        return i + 1
