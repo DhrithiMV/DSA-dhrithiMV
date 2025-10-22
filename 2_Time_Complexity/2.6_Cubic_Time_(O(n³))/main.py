df02d4157ab523084b1086809cfac22195013a1a
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:28:29 +0530

    Update main.py

--- 2_Time_Complexity/2.6_Cubic_Time_(O(n³))/main.py
+++ 2_Time_Complexity/2.6_Cubic_Time_(O(n³))/main.py
@@ -1,15 +1,5 @@
-# Cubic Time (O(n³))
-# Topic: Time & Space Complexity
-# Type: Home Challenge
-
-class Solution:
-    def allTriplets(self, arr: list[int]) -> list[tuple[int,int,int]]:
-        pass
-
-# Demo
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.allTriplets([1,2,3]))  # 27 triplets
-    print(sol.allTriplets([4,5]))    # 8 triplets
-    print(sol.allTriplets([9]))      # 1 triplet
-
+def print_triplets(arr):
+    for i in arr:
+        for j in arr:
+            for k in arr:
+                print((i, j, k))
