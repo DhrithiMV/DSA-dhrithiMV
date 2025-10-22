47a3d670b7bd2fe413beb51ef03bc5a74a9b7842
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:26:43 +0530

    Update main.py

--- 2_Time_Complexity/2.5_Quadratic_Time_(O(n²))/main.py
+++ 2_Time_Complexity/2.5_Quadratic_Time_(O(n²))/main.py
@@ -1,14 +1,4 @@
-# Quadratic Time (O(n²))
-# Topic: Time & Space Complexity
-# Type: Home Challenge
-
-class Solution:
-    def allPairs(self, arr: list[int]) -> list[tuple[int,int]]:
-        pass
-# Demo
-if __name__ == '__main__':
-    sol = Solution()
-    print(sol.allPairs([1,2,3]))  # Output: (1,1),(1,2),(1,3),(2,1),...
-    print(sol.allPairs([5,6]))    # Output: (5,5),(5,6),(6,5),(6,6)
-    print(sol.allPairs([7]))      # Output: (7,7)
-
+def print_pairs(arr):
+    for i in arr:
+        for j in arr:
+            print((i, j))
