d4349cbdfd8d7c9160dcd6b27d3c4a6afc875301
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 16:45:43 +0530

    Update main.py

--- 10_Lists_with_Two_Pointer_Techniques/10.2_Two_Sum_-_Sorted_Input/main.py
+++ 10_Lists_with_Two_Pointer_Techniques/10.2_Two_Sum_-_Sorted_Input/main.py
@@ -5,7 +5,21 @@
 
 class Solution:
     def twoSum(self, nums, target):
-        pass
+        left, right = 0, len(nums) - 1
+        while left < right:
+            curr_sum = nums[left] + nums[right]
+            if curr_sum == target:
+                return [left+1, right+1]
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2,7,11,15], 9))
+    print(sol.twoSum([1,2,3,4,4,9,56,90], 8))
+
 
 # Demo
 if __name__ == "__main__":
