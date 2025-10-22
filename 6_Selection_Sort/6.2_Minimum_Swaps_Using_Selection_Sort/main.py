70cbc7b512bf5a1d37943c4b78b39423c01c8666
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 15:47:40 +0530

    Implement minimumSwaps method with comment-free solution

--- 6_Selection_Sort/6.2_Minimum_Swaps_Using_Selection_Sort/main.py
+++ 6_Selection_Sort/6.2_Minimum_Swaps_Using_Selection_Sort/main.py
@@ -1,13 +1,21 @@
 # Minimum Swaps Using Selection Sort
 # Topic: Selection Sort
 # Type: Home Challenge
-
 class Solution:
-    def countSelectionSortSwaps(self, nums: list[int]) -> int:
-        pass
+    def minimumSwaps(self, nums):
+        swaps = 0
+        for i in range(len(nums)):
+            min_idx = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_idx]:
+                    min_idx = j
+            if min_idx != i:
+                nums[i], nums[min_idx] = nums[min_idx], nums[i]
+                swaps += 1
+        return swaps
 
 # Demo
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.countSelectionSortSwaps([64,25,12,22,11]))  
-    print(sol.countSelectionSortSwaps([1,2,3,4]))        
+    print(sol.minimumSwaps([64,25,12,22,11]))  
+    print(sol.minimumSwaps([1,2,3,4]))        
