86958a7274d6eda677e9760b1eb6f8294cb6ac9e
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 15:45:02 +0530

    Implement selection sort algorithm in main.py
    Implemented selection sort algorithm to sort an array.

--- 6_Selection_Sort/6.1_Sort_the_Array_Using_Selection_Sort/main.py
+++ 6_Selection_Sort/6.1_Sort_the_Array_Using_Selection_Sort/main.py
@@ -4,10 +4,16 @@
 
 class Solution:
     def selectionSort(self, nums: list[int]) -> list[int]:
-        pass
+        for i in range(len(nums)):
+            min_idx = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_idx]:
+                    min_idx = j
+            nums[i], nums[min_idx] = nums[min_idx], nums[i]
+        return nums
 
 # Demo
 if __name__ == '__main__':
     sol = Solution()
     print(sol.selectionSort([64,25,12,22,11]))  
-    print(sol.selectionSort([5,1,4,2,8]))     
+    print(sol.selectionSort([5,1,4,2,8]))
