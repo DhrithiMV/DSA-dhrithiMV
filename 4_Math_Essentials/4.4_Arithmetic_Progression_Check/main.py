34e755516c8c46a0ba065b74ae46dd7518f8a6e5
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 15:16:46 +0530

    Update main.py

--- 4_Math_Essentials/4.4_Arithmetic_Progression_Check/main.py
+++ 4_Math_Essentials/4.4_Arithmetic_Progression_Check/main.py
@@ -1,12 +1,17 @@
-# Arithmetic Progression Check
-# Topic: Math Essentials
-# Type: Home Challenge
-
 from typing import List
 
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
-        pass
+        arr.sort()
+        if len(arr) <= 2:
+            return True
+        
+        diff = arr[1] - arr[0]
+        for i in range(2, len(arr)):
+            if arr[i] - arr[i-1] != diff:
+                return False
+        
+        return True
 
 # Demo
 if __name__ == '__main__':
