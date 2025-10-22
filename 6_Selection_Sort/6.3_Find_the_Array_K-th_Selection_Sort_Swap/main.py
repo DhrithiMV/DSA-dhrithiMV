84ea869a3145ab4e9487ec9fa483e5b284149482
DhrithiMV <155285388+DhrithiMV@users.noreply.github.com>
2025-10-22 15:50:04 +0530

    Implement kthSwapSelectionSort solution in main.py

--- 6_Selection_Sort/6.3_Find_the_Array_K-th_Selection_Sort_Swap/main.py
+++ 6_Selection_Sort/6.3_Find_the_Array_K-th_Selection_Sort_Swap/main.py
@@ -1,11 +1,21 @@
 # Find the Array K-th Selection Sort Swap
 # Topic: Selection Sort
 # Type: Home Challenge
-
 class Solution:
-    def selectionSortKthSwap(self, nums: list[int], k: int) -> list[int]:
-        pass
+    def kthSwapSelectionSort(self, nums, k):
+        swaps = 0
+        for i in range(len(nums)):
+            min_idx = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[min_idx]:
+                    min_idx = j
+            if min_idx != i:
+                nums[i], nums[min_idx] = nums[min_idx], nums[i]
+                swaps += 1
+                if swaps == k:
+                    return nums
+        return nums
 # Demo
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.selectionSortKthSwap([64,25,12,22,11], 2)) 
+    print(sol.kthSwapSelectionSort([64,25,12,22,11], 2)) 
